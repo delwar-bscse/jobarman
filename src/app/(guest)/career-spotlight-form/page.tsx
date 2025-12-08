b55f16f81438a8df34e5c06f908a46e2ab46d2b9
jewelrana3 <juyelrana7752@gmail.com>
2025-12-08 12:55:12 +0600

    woorking form

---
+++ src/app/(guest)/career-spotlight-form/page.tsx
@@ -0,0 +1,10 @@
+import AdsCreateForm from "@/components/career-spotlightForm/AdsCreate";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <AdsCreateForm />
+    </div>
+  );
+}
